3fcab8a706818f5c3bccf49f7a5bbbcc1e5f3874
aishalewa <mustaphaaisha32@gmail.com>
2026-09-14 23:18:04 +0100

    Add DOM event listeners and game scoring

--- script.js
+++ script.js
@@ -2,27 +2,43 @@ let gameItems = ["rock","paper","scissors"];
 let humanScore = 0;
 let computerScore = 0; 
 
+let humanChoice;
+let computerChoice;
+
+let rock = document.querySelector('.rock');
+let paper = document.querySelector('.paper');
+let scissors = document.querySelector('.scissors');
+
+let resultContainer = document.querySelector(".result-container");
+let paragraph = document.createElement("p");
+resultContainer.appendChild(paragraph);
+
+
+
 function getComputerChoice(){
     return gameItems[Math.floor(Math.random() * 3)];
 }
 
-
-function getHumanChoice(){
-    let humanChoice = prompt('Enter ROCK, PAPER or SCISSORS');
-    return humanChoice.toLowerCase();
-    
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    paragraph.textContent = `Human Score : 0 : Computer Score : 0`
 }
 
 
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
+resetGame()
+
+let humanSelection;
+let computerSelection = getComputerChoice();
 
 
 
 function playRound(humanChoice, computerChoice) {
+    computerSelection = getComputerChoice();
     let message;
+    let winner;
     if (humanChoice === computerChoice){
-        message = "It's a tie"
+        message = "It's a tie!"
     }
     else if (
         //checks all conditions where the humanchoice win against the computerchoice
@@ -30,38 +46,56 @@ function playRound(humanChoice, computerChoice) {
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper") 
     ){
-        message = (`You win ${humanChoice} beats ${computerChoice}`)
+        message = (`You win this round! ${humanChoice} beats ${computerChoice}`)
         humanScore++;
+        if (humanScore == 5){
+            winner = "human";
+            alert (`Winner is ${winner}`);
+            resetGame();
+
+        }
     }
 
     else {
-        message = (`You lose ${computerChoice} beats ${humanChoice}`)
+        message = (`You lose this round ${computerChoice} beats ${humanChoice}`)
         computerScore++;
+        if (computerScore == 5){
+            winner = "computer"
+            alert (`Winner is ${winner}`);
+            resetGame();
+        }
     }
-    
+
     return message;
+    
 
 }
 
-function playGame(){
-    let round = 1;
-    while (round <= 5) {
+rock.addEventListener("click", function(){
+    humanSelection = "rock";
+    let message =  playRound(humanSelection, computerSelection);
+     playRound(humanSelection, computerSelection);
+    paragraph.innerHTML = ` ${message} <br> Human Score : ${humanScore} : Computer Score : ${computerScore}`
+    
+    
+});
 
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
+paper.addEventListener("click", function(){
+    humanSelection = "paper";
+    let message =  playRound(humanSelection, computerSelection);
+     playRound(humanSelection, computerSelection);
+    paragraph.innerHTML = ` ${message} <br> Human Score : ${humanScore} : Computer Score : ${computerScore}`
 
     
-        // console.log(humanSelection);
-        // console.log(computerSelection)
-        console.log(playRound(humanSelection, computerSelection));
-         round++;
-    };
-   
-    console.log(`humanScore is ${humanScore} : computerScore is ${computerScore}`);
+});
 
-}
+scissors.addEventListener("click", function(){
+    humanSelection = "scissors";
+    let message =  playRound(humanSelection, computerSelection);
+    playRound(humanSelection, computerSelection);
+    paragraph.innerHTML = ` ${message} <br> Human Score : ${humanScore} : Computer Score : ${computerScore}`
+
+    
+});
+
  
-
-
-
-playGame();
